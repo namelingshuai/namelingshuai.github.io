33c9c4c14fec533203035de7c103bda9b25b68e0
宋长帅 <songchangshuai@elf.one>
2020-05-08 17:42:41 +0800

    完整版

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ $(function(){
     new GridScrollFx( document.getElementById( 'grid' ), {
         viewportFactor : 0.4
     } );
-    $('#grid li').click(function(){
-        $('.t_show').show();
-        $('.t_images').show();
-        var _this = $(this).children().children('img').attr('src');
-        $('.t_img').css({'background': 'url('+ _this +') no-repeat','background-size':'100% 100%'})
-    });
-    $('.t_show').click(function(){
-        $('.t_show').hide();
-        $('.t_images').hide();
-    });
+    // $('#grid li').click(function(){
+    //     $('.t_show').show();
+    //     $('.t_images').show();
+    //     var _this = $(this).children().children('img').attr('src');
+    //     $('.t_img').css({'background': 'url('+ _this +') no-repeat','background-size':'100% 100%'})
+    // });
+    // $('.t_show').click(function(){
+    //     $('.t_show').hide();
+    //     $('.t_images').hide();
+    // });
 });
